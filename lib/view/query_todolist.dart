fd30653f3329b7443b5e994ee765ceb566e423bf
인성 <twapet0124@gmail.com>
2026-09-17 19:02:09 +0900

    lib update

---
+++ lib/view/query_todolist.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QueryTodolist extends StatefulWidget {
+  const QueryTodolist({super.key});
+
+  @override
+  State<QueryTodolist> createState() => _QueryTodolistState();
+}
+
+class _QueryTodolistState extends State<QueryTodolist> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
